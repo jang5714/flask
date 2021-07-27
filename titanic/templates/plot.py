5082ac9b2ebe2e1f2fb3ca9f18066b347b19e96e
jang5714 <lionx3@naver.com>
2021-07-27 17:39:09 +0900

    july0727

--- titanic/templates/plot.py
+++ titanic/templates/plot.py
@@ -22,7 +22,10 @@ class Plot (object):
     def show_plot_survived_dead(self):
         this = self.df
         f, ax = plt.subplots(1, 2, figsize=(18, 8)) # 1행 2열
-        this['Survived'].value_counts().plot.pie(explode=[0,0.1], autopct='%1.1f%%', ax=ax[0], shadow=True )
+        series = this['Survived'].value_counts()
+        print(type(series))
+        print(series)
+        series.plot.pie(explode=[0,0.1], autopct='%1.1f%%', ax=ax[0], shadow=True )
         ax[0].set_title('0.사망자 vs 1.생존자')
         ax[0].set_ylabel('')
         ax[1].set_title('0.사망자 vs 1.생존자')
@@ -30,3 +33,17 @@ class Plot (object):
         plt.show()
 
 
+    def show_plot_pclass(self):
+        this = self.df
+        this['생존결과'] = this['Survived'].replace(0,'사망자').replace(1,'생존자')
+        this['좌석등급'] = this['Pclass'].replace(1, '1등석').replace(2, '2등석').replace(3, '3등석')
+        sns.countplot(data=this, x='좌석등급', hue='생존결과')
+        plt.show()
+
+    def show_plot_embarked(self):
+        # C: 쉘버그, S: 사우스행튼 Q: 퀸즈타운
+        this = self.df
+        this['생존결과'] = this['Survived'].replace(0, '사망자').replace(1, '생존자')
+        this['승선항구']= this['Embarked'].replace('C', '쉘버그').replace('S', '사우스햄튼').replace('Q', '퀸즈타운')
+        sns.countplot(data=this, x='승선항구', hue='생존결과')
+        plt.show()
